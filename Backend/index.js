6e58aa92de4bd87f31931e229448b2beb9f27b0f
Shamsullah <114299329+Shamsullah77@users.noreply.github.com>
2024-07-02 03:39:13 +0430

    Error is fixed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const { FORCE } = require("sequelize/lib/index-hints");
+const { PythonShell } = require("python-shell");
 const sequelize = require("./Config/db");
 const User = require("./models/users");
 const location = require("./models/location");
@@ -10,32 +12,30 @@ const CrimeType = require("./models/crimeType");
 const Crimes = require("./models/crimes");
 const userfeedback = require("./models/userfeedback");
 // const Crimefromothersource = require("./models/crimefromothersource");
-
-const userroutes = require("./routes/userRoutes");
-const { FORCE } = require("sequelize/lib/index-hints");
-const { PythonShell } = require("python-shell");
-
 const predictRoute = require("./routes/predictionRoute");
+const userroutes = require("./routes/userRoutes");
 
-app.use("/predict", predictRoute);
+
+const PORT = process.env.PORT || 3002;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const PORT = process.env.PORT || 3002;
 app.use(userroutes);
+app.use("/predict", predictRoute);
 
 // Test database connection
 sequelize
-  .authenticate()
-  .then(() => console.log("Database connected..."))
-  .catch((err) => console.log("Error: " + err));
+.authenticate()
+.then(() => console.log("Database connected..."))
+.catch((err) => console.log("Error: " + err));
 
 // Sync database
 sequelize
-  .sync()
-  .then(() => console.log("Database Connected..."))
-  .catch((err) => console.log("Error: " + err));
+.sync()
+.then(() => console.log("Database Connected..."))
+.catch((err) => console.log("Error: " + err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
